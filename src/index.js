b9dae0ddfc4a522dcf3e789037187fdf833b43e0
Inderpal Singh <singhinther@gmail.com>
2017-03-13 21:16:50 +0100

    linting is done during compilation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ type Plugin = { term: string, display: (result: Object) => void, actions: Object
 
 const plugin = ({ term, display, actions }: Plugin) => {
   console.log(term);
+  console.log(display);
+  console.log(actions);
 };
 
 module.exports = {
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,51 @@
-const webpack = require('webpack');
 const path = require('path');
 const FlowWebpackPlugin = require('flow-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/index'
+    index: './src/index',
   },
   output: {
     path: './dist',
     libraryTarget: 'commonjs2',
-    filename: 'index.js'
+    filename: 'index.js',
   },
   resolve: {
     extensions: ['.js'],
     modules: [
       path.resolve('./src'),
       path.resolve('./node_modules'),
-    ]
+    ],
   },
   target: 'electron-renderer',
   module: {
     rules: [{
       test: /\.jsx?$/,
       use: {
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }, {
       test: /\.css$/,
       use: [{
-        loader: 'style-loader'
+        loader: 'style-loader',
       }, {
         loader: 'css-loader',
         query: {
-          modules: true
-        }
-      }]
+          modules: true,
+        },
+      }],
     }, {
       test: /\.png$/,
       use: {
-        loader: 'url-loader'
-      }
-    }]
+        loader: 'url-loader',
+      },
+    }, {
+      enforce: 'pre',
+      test: /\.(js|jsx)$/,
+      loader: 'eslint-loader',
+      exclude: /(node_modules)/,
+    }],
   },
   plugins: [
     new FlowWebpackPlugin(),
